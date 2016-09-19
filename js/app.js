c230f265d2c23eac4f2623c705e0a5924e9978a4
sacunao <sheila.aobregon@gmail.com>
2016-09-19 15:56:07 -0500

    primera version del proyecto

---
+++ js/app.js
@@ -0,0 +1,22 @@
+window.addEventListener("load", function() {
+	var boton = document.getElementById("tweetear");
+	boton.addEventListener("click", function() {
+		var texto = document.getElementById("textBox").value;
+		newTweet(texto);
+        document.getElementById("textBox").value = "";
+
+	});
+
+	function newTweet (texto) {
+        var tweet = document.createElement("div");
+        tweet.className = "tweetDiv";
+        var contenedor = document.getElementById("contenedor");
+        tweet.innerText = texto;
+        
+        if(!contenedor.childNodes[0]){
+            contenedor.appendChild(tweet);
+        } else {
+            contenedor.insertBefore(tweet,contenedor.childNodes[0]);
+        }
+	}
+});
---
+++ readme.md
@@ -0,0 +1,9 @@
+##EJERCCIO TWITTER-BOX
+Tener un contenedor vacío que acumule los mensajes
+1. Formulario: TextArea y Botón.
+2. Evento para el click del botón 
+3. Dentro del evento, obtener el valor del textarea.
+4. Darle formato al texto del TextArea.
+5. Agregar el texto formateado al contenedor.
+
+![Imagen]( "Imagen")

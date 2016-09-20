eff8d0ae3a337ced9f76643df03b59eb88b35b1c
sacunao <sheila.aobregon@gmail.com>
2016-09-20 17:10:38 -0500

    agregando la hora en tweet

--- js/app.js
+++ js/app.js
@@ -1,37 +1,52 @@
 window.addEventListener("load", function() {
     var color = ["violet","turquesa","red"];
     var maximo = [120, 130, 140];
-
 	var boton = document.getElementById("tweetear");
     boton.disabled = true; //Por defecto
+
 	boton.addEventListener("click", function(e) {
         e.preventDefault();
-        var textArea = document.getElementById("textBox");
-		var texto = textArea.value.trim();
-		newTweet(texto);
-        textArea.value = "";
+        //var textArea = document.getElementById("textBox");
+		var texto = document.getElementById("textBox");
+		newTweet(texto.value);
+        texto.value = "";
         contador.textContent = "140";
-        boton.disabled = false;
+        boton.disabled = true;
 
 	});
 
 	function newTweet (texto) {
         var tweet = document.createElement("div");
+        tweet.innerHTML = texto;
         tweet.className = "tweetDiv";
         var contenedor = document.getElementById("contenedor");
-        tweet.innerText = texto;
-        
+
         if(!contenedor.childNodes[0]){
             contenedor.appendChild(tweet);
         } else {
             contenedor.insertBefore(tweet,contenedor.childNodes[0]);
         }
+        agregarHora(tweet);
 	}
-    
+    function agregarHora(nuevo){
+        var fecha = new Date();
+        var hora = fecha.getHours();
+        var minuto = fecha.getMinutes();
+            if (minuto < 10) {
+                minuto = "0" + minuto;
+            }
+        var horaImprimible = hora + " : " + minuto + " " ;
+        var hora = document.createElement("span");
+        hora.innerText = horaImprimible;
+
+        nuevo.insertBefore(hora,nuevo.childNodes[0]);  
+    }
+
+
     textBox.addEventListener("keyup", function() {
         boton.disabled = false;
         countAndColor(maximo, color);
-        countEnter();
+        countEnter(text);
 	});
     
     
@@ -60,10 +75,10 @@ window.addEventListener("load", function() {
         }     
     } 
     
-    function countEnter(){
-        var text = document.getElementById("textBox");
-        var textSinEspacios = text.value.trim().length;
-        var espacios = text.value.match(/\n/g);
+    function countEnter(text){
+        var text = document.getElementById("textBox").value;
+        var textSinEspacios = text.trim().length;
+        var espacios = text.match(/\n/g);
         var cantEnters = espacios.length;
         
         if(textSinEspacios === 0){
@@ -80,10 +95,8 @@ window.addEventListener("load", function() {
     function autosize(){
         var el = this;
         setTimeout(function(){
-        el.style.cssText = 'height:auto; padding:0';
-        // for box-sizing other than "content-box" use:
-        // el.style.cssText = '-moz-box-sizing:content-box';
-        el.style.cssText = 'height:' + el.scrollHeight + 'px';
+            el.style.cssText = 'height:auto; padding:0';
+            el.style.cssText = 'height:' + el.scrollHeight + 'px';
         },0);
     }
 });

970a302708bd1ef6a9d550acf381f07fb7c67723
sacunao <sheila.aobregon@gmail.com>
2016-09-19 20:54:01 -0500

    contador de carateres y cambio de color al de 140

--- js/app.js
+++ js/app.js
@@ -1,14 +1,13 @@
 window.addEventListener("load", function() {
 	var boton = document.getElementById("tweetear");
+    boton.disabled = true; //Por defecto
 	boton.addEventListener("click", function(e) {
         e.preventDefault();
-        var textArea = document.getElementById("textBox")
+        var textArea = document.getElementById("textBox");
 		var texto = textArea.value;
 		newTweet(texto);
         textArea.value = "";
-
 	});
-
 	function newTweet (texto) {
         var tweet = document.createElement("div");
         tweet.className = "tweetDiv";
@@ -21,4 +20,21 @@ window.addEventListener("load", function() {
             contenedor.insertBefore(tweet,contenedor.childNodes[0]);
         }
 	}
+    textBox.addEventListener("keypress", function() {
+        boton.disabled = false;
+        count();
+	});
+     function count(maximo){
+        var maximo = 140;
+        var textValue = document.getElementById("textBox").value
+        var largo = textValue.length;
+        var contador =  document.getElementById("contador");
+        contador.innerHTML = maximo-largo;
+        if(largo >= maximo) { 
+              contador.classList.add("red");
+              boton.disabled = true; 
+        } else { 
+              contador.classList.remove("red");
+        }   
+     }    
 });
